897c3293209cdb0fdec297a0c5763acad8023582
matm01 <mumm.mathias@gmail.com>
2024-02-01 11:56:58 +0100

    add dataloader

---
+++ dataloader.py
@@ -0,0 +1,18 @@
+import pandas as pd
+def dataloader():
+    """
+    Load the dataset from CSV files.
+
+    Returns:
+    X_train: pandas DataFrame
+        Training dataset values
+    y_train: pandas DataFrame
+        Training dataset labels
+    X_test: pandas DataFrame
+        Test dataset values
+    """
+    # Load the dataset
+    X_train = pd.read_csv('data/test_values.csv')
+    y_train = pd.read_csv('data/train_labels.csv')
+    X_test = pd.read_csv('data/test_values.csv')
+    return X_train, y_train, X_test
