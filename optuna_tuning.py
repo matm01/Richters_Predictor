0ef321af4042d407bc0e9f6591c6871e49f24bac
matm01 <mumm.mathias@gmail.com>
2024-02-03 07:25:07 +0100

    added docstrings

--- optuna_tuning.py
+++ optuna_tuning.py
@@ -1,16 +1,20 @@
 """
 Optuna search that optimizes a classifier configuration for Richters dataset
 using XGBoost.
-
 """
 
 import numpy as np
 import optuna
-from sklearn.model_selection import train_test_split
 import xgboost as xgb
 from sklearn.metrics import f1_score
 
 def transform_data(X_train, y_train, X_valid, y_valid, preprocessor):
+    """Transforms the training and validation data using the provided preprocessor.
+
+    Returns:
+    tuple: Transformed training input samples, transformed training target values, transformed validation input samples 
+    and transformed validation target values.
+    """
     opt_X_train = preprocessor.fit_transform(X_train, y_train['damage_grade'])
     opt_X_valid = preprocessor.transform(X_valid)
     opt_y_train = y_train['damage_grade'].values
@@ -21,14 +25,26 @@ def transform_data(X_train, y_train, X_valid, y_valid, preprocessor):
 
 
 def do_study(X_train, y_train, X_valid, y_valid, preprocessor):
+    """This function does an automatic hyperparameter optimization.
 
-    opt_X_train, opt_y_train, opt_X_valid, opt_y_valid = transform_data(X_train, y_train, X_valid, y_valid, preprocessor)
-    dtrain = xgb.DMatrix(opt_X_train, label=opt_y_train)
-    dvalid = xgb.DMatrix(opt_X_valid, label=opt_y_valid)
+    It takes training and validation Data and a preprocessor pipeline as input. The function transforms the data, 
+    defines an objective function for hyperparameter optimization, creates an Optuna study, and then optimizes the 
+    objective function over a specified number of trials.
+
+    Returns: 
+    dict: The best Parameters are returned as the output of the function.
+    """
+    trf_X_train, trf_y_train, trf_X_valid, trf_y_valid = transform_data(X_train, y_train, X_valid, y_valid, preprocessor)
+    dtrain = xgb.DMatrix(trf_X_train, label=trf_y_train)
+    dvalid = xgb.DMatrix(trf_X_valid, label=trf_y_valid)
 
     def objective(trial):
-
-        """Define the objective function"""
+        """This function defines the objective to be optimized. 
+        It takes a 'trial' object as input and returns the accuracy of the model. The 'trial' object is used to suggest hyperparameters 
+        for the XGBoost model. The function sets various parameters such as learning rate, booster type, max depth, gamma, etc. for the XGBoost model. 
+        It then trains the model on the suggested parameters and evaluates its accuracy using the F1 score. The accuracy value is returned 
+        as the output of the function.
+        """
         params = {
             'learning_rate': trial.suggest_loguniform('learning_rate', 1e-3, 1e-1),
             "booster": trial.suggest_categorical("booster", ["gbtree", "dart"]),
@@ -45,7 +61,7 @@ def do_study(X_train, y_train, X_valid, y_valid, preprocessor):
         bst = xgb.train(params, dtrain)
         preds = bst.predict(dvalid)
         pred_labels = np.rint(preds)
-        accuracy = f1_score(opt_y_valid, pred_labels, average='micro')
+        accuracy = f1_score(trf_y_valid, pred_labels, average='micro')
         
         return accuracy
 
@@ -60,4 +76,4 @@ def do_study(X_train, y_train, X_valid, y_valid, preprocessor):
     print("  Params: ")
     for key, value in trial.params.items():
         print("    {}: {}".format(key, value))
-    return trial.params
+    return trial
