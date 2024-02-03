db8c23d165205719cbc7bf197bbf173763e95590
matm01 <mumm.mathias@gmail.com>
2024-02-03 15:17:31 +0100

    added docstring

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -1,5 +1,15 @@
 import pandas as pd
 def clean_data(X_train, y_train):
+    """
+    Clean the input data by merging X_train and y_train on 'building_id' and removing outlier rows where 'age' has values > 900. 
+    Then split X_train and y_train again and return the cleaned X_train and y_train.
+    Parameters:
+    - X_train: the input training data
+    - y_train: the target training data
+    Return:
+    - X_train: the cleaned input training data
+    - y_train: the cleaned target training data
+    """
 
     merged_df = pd.merge(X_train, y_train, on='building_id', how='left')
 
